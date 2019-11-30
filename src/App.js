632abc783f077dcea8771f8400a48bab46b83414
Michael Goodrum <michael@vlipsy.com>
2019-11-29 19:07:01 -0500

    Add display toggle for zombies on main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import ZombieList from './components/ZombieList';
+import Selector from './components/Selector';
 
 function App(props) {
   return (
     <div className="App">
       <header className="App-header">
+      <BrowserRouter>
+        <div>
+          <Route path='/:location?' render={({ match }) => (
+            <ZombieList location={match.params.location} />
+          )} />
+          <Selector />
+        </div>
+      </BrowserRouter>
       </header>
     </div>
   );
-}
+};
 
-export default App
+export default App;
